98856e2dc36d799ee710f66ba2cdd988b70770e2
Gabriel-Henrique-Selani-Barbosa <gabrielbarbosa61@gmail.com>
2022-09-04 18:53:09 -0300

    back

--- server/index.js
+++ server/index.js
@@ -46,8 +46,12 @@ app.post("/register", (req, res) => {
           }
         })
       })
+      actual_schema.query("CREATE TABLE tb_client (client_id INT AUTO_INCREMENT PRIMARY KEY, cpf_cnpj VARCHAR(50) NOT NULL, nome VARCHAR(50), endereco VARCHAR(50), num INT, cidade VARCHAR (50), estado VARCHAR(50))")
+      actual_schema.query("CREATE TABLE tb_fornecedor (id_fornecedor INT AUTO_INCREMENT PRIMARY KEY, cpf_cnpj VARCHAR(50) NOT NULL, nome VARCHAR(50), produto VARCHAR(50))")
+      actual_schema.query(`CREATE TABLE tb_equip (equip_id INT AUTO_INCREMENT PRIMARY KEY, CONSTRAINT id_fornecedor FOREIGN KEY(equip_id) REFERENCES ${nome_empresa}.tb_fornecedor (id_fornecedor), categoria VARCHAR(50) NOT NULL, marca VARCHAR(50), modelo VARCHAR(50), preco INT)`)
+      actual_schema.query("CREATE TABLE tb_pedido (pedido_id INT AUTO_INCREMENT PRIMARY KEY, servico VARCHAR(50), valor INT, data_pedido DATE, observação VARCHAR (50))")
     });
-  })  
+  }) 
 });
 
 app.post("/login", (req, res) => {
@@ -138,35 +142,215 @@ app.post("registrar-obra", (req, res) => {
   db.query("INSERT INTO obra (nomeobra, cliente, situacao, categoria) VALUE (?, ?, ?, ?)", [nomeobra, cliente, situacao, categoria])
 })
 
-app.post("registrar-fornecedor", (req, res) => {
-  const nome_empresa = req.body.nome_empresa;
+app.get("/get-pedidos", (req, res) => {
+  const nome_empresa = req.query.storeName;
   const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1574857",
     database: `${nome_empresa}`,
   });
+  db.query("SELECT * FROM tb_pedido", (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send(result)
+    }
+  });
+});
+
+app.post("/delete-pedidos", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const pedido_id = req.body.pedido_id
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("DELETE FROM tb_pedido WHERE pedido_id = ?", [pedido_id], (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send("Cliente fornecedor com sucesso")
+    }
+  })
 })
 
-app.post("registrar-cliente", (req, res) => {
-  const nome_empresa = req.body.nome_empresa;
+app.post("/registrar-pedidos", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const servico = req.body.servico;
+  const valor = req.body.valor;
+  const data_pedido = req.body.datapedido;
+  const observacao = req.body.observacao;
   const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1574857",
     database: `${nome_empresa}`,
   });
+  db.query("INSERT INTO tb_pedido (servico, valor, data_pedido, observação) VALUE (?, ?, ?, ?)", [servico, valor, data_pedido, observacao])
 })
 
-app.post("registrar-fornecedor", (req, res) => {
-  const nome_empresa = req.body.nome_empresa;
+app.get("/get-equipamentos", (req, res) => {
+  const nome_empresa = req.query.storeName;
   const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1574857",
     database: `${nome_empresa}`,
   });
+  db.query("SELECT * FROM tb_equip", (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send(result)
+    }
+  });
+});
+
+app.post("/delete-equipamentos", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const id_equip = req.body.equip_id
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("DELETE FROM tb_equip WHERE equip_id = ?", [id_equip], (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send("Cliente fornecedor com sucesso")
+    }
+  })
 })
+
+app.post("/registrar-equipamentos", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const categoria = req.body.categoria;
+  const marca = req.body.marca;
+  const modelo = req.body.modelo;
+  const preco = req.body.preco;
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("INSERT INTO tb_equip (categoria, marca, modelo, preco) VALUE (?, ?, ?, ?)", [categoria, marca, modelo, preco])
+})
+
+app.get("/get-fornecedores", (req, res) => {
+  const nome_empresa = req.query.storeName;
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("SELECT * FROM tb_fornecedor", (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send(result)
+    }
+  });
+});
+
+app.post("/delete-fornecedores", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const id_fornecedor = req.body.cliente_id
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("DELETE FROM tb_fornecedor WHERE id_fornecedor = ?", [id_fornecedor], (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send("Cliente fornecedor com sucesso")
+    }
+  })
+})
+
+app.post("/registrar-fornecedores", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const nome = req.body.nome;
+  const cnpjcpf = req.body.cnpjcpf;
+  const produto = req.body.produto;
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("INSERT INTO tb_fornecedor (cpf_cnpj, nome, produto) VALUE (?, ?, ?)", [cnpjcpf, nome, produto])
+})
+
+app.get("/get-clientes", (req, res) => {
+  const nome_empresa = req.query.storeName;
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("SELECT * FROM tb_client", (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send(result)
+    }
+  });
+});
+
+app.post("/delete-clientes", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const cliente_id = req.body.cliente_id
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("DELETE FROM tb_client WHERE client_id = ?", [cliente_id], (err, result) => {
+    if(err) {
+      res.send(err)
+    }
+    if(result) {
+      res.send("Cliente deletado com sucesso")
+    }
+  })
+})
+
+app.post("/registrar-cliente", (req, res) => {
+  const nome_empresa = req.body.storeName;
+  const nome = req.body.nome;
+  const cnpjcpf = req.body.cnpjcpf;
+  const estado = req.body.estado;
+  const cidade = req.body.cidade;
+  const endereco = req.body.endereco;
+  const numero = req.body.numero;
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1574857",
+    database: `${nome_empresa}`,
+  });
+  db.query("INSERT INTO tb_client (cpf_cnpj, nome, endereco, num, cidade, estado) VALUE (?, ?, ?, ?, ?, ?)", [cnpjcpf, nome, endereco, numero, cidade, estado])
+})
+
 app.listen(3001, () => {
   console.log("rodando na porta 3001");
 });
